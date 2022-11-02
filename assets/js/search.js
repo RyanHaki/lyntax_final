a126148701daebfef699bbb9a8e0ede4c8482369
Soham360 <Soham360@users.noreply.github.com>
2022-11-02 04:10:25 +0000

    deploy: 2c19d16634da9ef2b07d69967cb4b8ed1a3edbe5

--- assets/js/search.js
+++ assets/js/search.js
@@ -47,7 +47,7 @@ jtd.onReady = function(ready) {
 
 function initSearch() {
     var request = new XMLHttpRequest();
-    request.open('GET', '/APCSPwebPage/assets/js/search-data.json', true);
+    request.open('GET', '/lyntax_final/assets/js/search-data.json', true);
   
     request.onload = function(){
       if (request.status >= 200 && request.status < 400) {
